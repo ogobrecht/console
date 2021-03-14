4394ef72a494fd5acf79fb57d1db66d9d2d79cd9
Ottmar Gobrecht <ogobrecht@users.noreply.github.com>
2021-03-14 16:01:10 +0100

    add cleanup job

--- uninstall/drop_console_objects.sql
+++ uninstall/drop_console_objects.sql
@@ -15,11 +15,23 @@ declare
   v_ddl          varchar2 (100 char);
 begin
 
+  --cleanup job
+  for i in (
+    select 'begin sys.dbms_scheduler.drop_job(job_name => ''' || job_name || ''', force => true); end;' as ddl
+      from user_scheduler_jobs
+     where job_name = 'CONSOLE_CLEANUP' )
+  loop
+    dbms_output.put_line('- ' || i.ddl);
+    execute immediate i.ddl;
+    v_object_count := v_object_count + 1;
+  end loop;
+
   --package body
-  for i in (select 'drop ' || lower(object_type) || ' ' || object_name as ddl
-              from user_objects
-             where object_type = 'PACKAGE BODY'
-               and object_name = 'CONSOLE')
+  for i in (
+    select 'drop ' || lower(object_type) || ' ' || object_name as ddl
+      from user_objects
+     where object_type = 'PACKAGE BODY'
+       and object_name = 'CONSOLE')
   loop
     dbms_output.put_line('- ' || i.ddl);
     execute immediate i.ddl;
@@ -27,10 +39,11 @@ begin
   end loop;
 
   --package spec
-  for i in (select 'drop ' || lower(object_type) || ' ' || object_name as ddl
-              from user_objects
-             where object_type = 'PACKAGE'
-               and object_name = 'CONSOLE')
+  for i in (
+    select 'drop ' || lower(object_type) || ' ' || object_name as ddl
+      from user_objects
+     where object_type = 'PACKAGE'
+       and object_name = 'CONSOLE')
   loop
     dbms_output.put_line('- ' || i.ddl);
     execute immediate i.ddl;
@@ -42,26 +55,24 @@ begin
     select 'drop table ' || table_name || ' cascade constraints' as ddl,
            table_name
       from user_tables
-     where table_name in ('CONSOLE_LOGS', 'CONSOLE_SESSIONS', 'CONSOLE_LEVELS') )
-     -- FIXME: Remove obsolete CONSOLE_LEVELS, when we have a stable version.
+     where table_name in ('CONSOLE_LOGS', 'CONSOLE_SESSIONS') )
   loop
-    --> FIXME: Do the following check, when we have a stable version available.
-    --execute immediate 'select count(*) from ' || i.table_name ||
-    --  case when i.table_name = 'CONSOLE_LOGS'
-    --    then q'{ where log_level = 0 and message not like '{o,o} CONSOLE%' }'
-    --    else null
-    --  end
-    --  into v_count;
-    --if i.table_name = 'CONSOLE_LOGS' and v_count > 0 then
-    --  dbms_output.put_line(
-    --    '- NOTE: ' || i.table_name ||
-    --    ' contains important user data - please review and drop it by youself (' ||
-    --    i.ddl || ')');
-    --else
+    execute immediate 'select count(*) from ' || i.table_name ||
+      case when i.table_name = 'CONSOLE_LOGS'
+        then q'{ where level_id = 0 and message not like '{o,o} CONSOLE%' }'
+        else null
+      end
+      into v_count;
+    if i.table_name = 'CONSOLE_LOGS' and v_count > 0 then
+      dbms_output.put_line(
+        '- NOTE: ' || i.table_name ||
+        ' contains important user data - please review and drop it by youself (' ||
+        i.ddl || ')');
+    else
       dbms_output.put_line('- ' || i.ddl);
       execute immediate i.ddl;
       v_object_count := v_object_count + 1;
-    --end if;
+    end if;
   end loop;
 
   dbms_output.put_line('- ' || v_object_count || ' object' || case when v_object_count != 1 then 's' end || ' dropped');
