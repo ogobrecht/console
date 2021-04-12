8e77787b312a3eec5eed93a1aa72c6b3bd2540d4
Ottmar Gobrecht <ogobrecht@users.noreply.github.com>
2021-04-12 10:02:20 +0200

    Update performance.sql

--- tests/performance.sql
+++ tests/performance.sql
@@ -14,24 +14,6 @@ end;
 /
 
 prompt
-prompt 1.000 utl_load_session_configuration CALLS
-declare
-  v_iterator   pls_integer := 1000;
-  v_start      timestamp;
-  v_rt         number;
-  v_result     varchar2(100);
-begin
-  v_start := localtimestamp;
-  for i in 1 .. v_iterator loop
-    console.utl_load_session_configuration;
-  end loop;
-  v_rt := console.get_runtime_seconds(v_start);
-  console.print( '- runtime all : ' || trim(to_char(v_rt, '0.000000000')) || ' seconds' );
-  console.print( '- per call    : ' || trim(to_char(v_rt/1000, '0.000000000')) || ' seconds' );
-end;
-/
-
-prompt
 prompt 100.000 LOG CALLS IN LEVEL ERROR (how many time you loose by do nothing)
 declare
   v_iterator   pls_integer := 100000;
@@ -58,10 +40,10 @@ begin
   end loop;
   v_rt_console := console.get_runtime_seconds(v_start);
   --
-  console.print( '- empty loop  : ' || trim(to_char(v_rt_null,    '0.000000')) || ' seconds (it does not really matter)' );
-  console.print( '- logger.log  : ' || trim(to_char(v_rt_logger,  '0.000000')) || ' seconds' );
-  console.print( '- console.log : ' || trim(to_char(v_rt_console, '0.000000')) || ' seconds' );
-  console.print( '- factor      : ' || trim(to_char(v_rt_logger/v_rt_console, '90.0')));
+  console.print( '- empty loop     : ' || trim(to_char(v_rt_null,    '0.000000')) || ' seconds (it does not really matter)' );
+  console.print( '- logger.log     : ' || trim(to_char(v_rt_logger,  '0.000000')) || ' seconds' );
+  console.print( '- console.log    : ' || trim(to_char(v_rt_console, '0.000000')) || ' seconds' );
+  console.print( '- factor         : ' || trim(to_char(v_rt_logger/v_rt_console, '90.0')));
 end;
 /
 
@@ -84,30 +66,36 @@ begin
     console.count('test');
   end loop;
   v_rt_count := console.get_runtime_seconds(v_start);
-  console.print( '- count result  : ' || (to_char(console.count_end('test'))));
-  console.print( '- runtime log   : ' || trim(to_char(v_rt_log, '0.000000')) || ' seconds' );
-  console.print( '- runtime count : ' || trim(to_char(v_rt_count, '0.000000')) || ' seconds' );
-  console.print( '- factor        : ' || trim(to_char(v_rt_log/v_rt_count, '90.0')));
+  console.print( '- count result   : ' || (to_char(console.count_end('test'))));
+  console.print( '- console.log    : ' || trim(to_char(v_rt_log,   '0.000000')) || ' seconds' );
+  console.print( '- console.count  : ' || trim(to_char(v_rt_count, '0.000000')) || ' seconds' );
+  console.print( '- factor         : ' || trim(to_char(v_rt_log/v_rt_count, '90.0')));
 end;
 /
 
 prompt
 prompt 100.000 TIME CALLS IN LEVEL ERROR (how many time you loose by measuring time)
 declare
-  v_iterator   pls_integer := 100000;
-  v_start      timestamp;
-  v_rt         number;
-  v_result     varchar2(100);
+  v_iterator pls_integer := 100000;
+  v_start    timestamp;
+  v_rt_log   number;
+  v_rt_time  number;
+  v_result   varchar2(100);
 begin
   v_start := localtimestamp;
   for i in 1 .. v_iterator loop
-    console.time('test');
-    --v_result := console.time_end('test');
+    console.log('test');
   end loop;
-  v_rt := console.get_runtime_seconds(v_start);
-  --console.print( '- last runtime    : ' || v_result);
-  console.print( '- runtime all     : ' || trim(to_char(v_rt, '0.000000')) || ' seconds' );
-  console.print( '- per measurement : ' || trim(to_char(v_rt/1000, '0.000000')) || ' seconds' );
+  v_rt_log := console.get_runtime_seconds(v_start);
+  --
+  v_start := localtimestamp;
+  for i in 1 .. v_iterator loop
+    console.time('test');
+  end loop;
+  v_rt_time := console.get_runtime_seconds(v_start);
+  console.print( '- console.log    : ' || trim(to_char(v_rt_log,  '0.000000')) || ' seconds' );
+  console.print( '- console.time   : ' || trim(to_char(v_rt_time, '0.000000')) || ' seconds' );
+  console.print( '- factor         : ' || trim(to_char(v_rt_log/v_rt_time, '90.0')));
 end;
 /
 
@@ -145,9 +133,9 @@ begin
   console.flush_cache;
   v_rt_console := console.get_runtime_seconds(v_start);
   -- print results
-  console.print( '- logger.log  : ' || trim(to_char(v_rt_logger,  '0.000000')) || ' seconds' );
-  console.print( '- console.log : ' || trim(to_char(v_rt_console, '0.000000')) || ' seconds' );
-  console.print( '- factor      : ' || trim(to_char(v_rt_logger/v_rt_console, '90.0')));
+  console.print( '- logger.log     : ' || trim(to_char(v_rt_logger,  '0.000000')) || ' seconds' );
+  console.print( '- console.log    : ' || trim(to_char(v_rt_console, '0.000000')) || ' seconds' );
+  console.print( '- factor         : ' || trim(to_char(v_rt_logger/v_rt_console, '90.0')));
   --configure logger and console
   logger.set_level(logger.g_error);
   console.exit;
@@ -188,158 +176,177 @@ declare
   v_iterator pls_integer := 1000;
   v_start    timestamp;
   v_scope    varchar2(1000);
-  v_rt_scope number;
+  v_rt       number;
 begin
   v_start := localtimestamp;
   for i in 1 .. v_iterator loop
     v_scope := console.get_scope;
   end loop;
-  v_rt_scope := console.get_runtime_seconds(v_start);
+  v_rt := console.get_runtime_seconds(v_start);
   --
-  console.print( '- get_scope : ' || trim(to_char(v_rt_scope, '0.000000')) || ' seconds' );
+  console.print( '- get_scope      : ' || trim(to_char(v_rt, '0.000000')) || ' seconds' );
 end;
 /
 
+/*
+prompt
+prompt 1.000 utl_load_session_configuration CALLS
+declare
+  v_iterator   pls_integer := 1000;
+  v_start      timestamp;
+  v_rt         number;
+  v_result     varchar2(100);
+begin
+  v_start := localtimestamp;
+  for i in 1 .. v_iterator loop
+    console.utl_load_session_configuration;
+  end loop;
+  v_rt := console.get_runtime_seconds(v_start);
+  console.print( '- runtime all    : ' || trim(to_char(v_rt,      '0.000000000')) || ' seconds' );
+  console.print( '- per call       : ' || trim(to_char(v_rt/1000, '0.000000000')) || ' seconds' );
+end;
+/
 
---prompt
---prompt TIMESTAMP > DATE - 100.000 CALLS
---declare
---  v_iterator pls_integer := 100000;
---  v_start    timestamp;
---  v_end_time timestamp   := systimestamp + 1/24/60*10;
---  v_end_date date        := sysdate      + 1/24/60*10;
---  v_rt_time  number;
---  v_rt_date  number;
---begin
---  v_start := localtimestamp;
---  for i in 1 .. v_iterator loop
---    if systimestamp <= v_end_time then null; end if;
---  end loop;
---  v_rt_time := console.get_runtime_seconds(v_start);
---  --
---  v_start := localtimestamp;
---  for i in 1 .. v_iterator loop
---    if sysdate <= v_end_date then null; end if;
---  end loop;
---  v_rt_date := console.get_runtime_seconds(v_start);
---  --
---  console.print( '- timestamp   : ' || trim(to_char(v_rt_time, '0.000000')) || ' seconds' );
---  console.print( '- date        : ' || trim(to_char(v_rt_date, '0.000000')) || ' seconds' );
---  console.print( '- factor      : ' || trim(to_char(v_rt_time/v_rt_date, '90.0')));
---end;
---/
---
---prompt
---prompt DATE > GET_TIME - 100.000 CALLS
---declare
---  v_iterator pls_integer := 100000;
---  v_start    timestamp;
---  v_end_date date        := sysdate               + 1/24/60*10;
---  v_end_time pls_integer := dbms_utility.get_time + 100 * 10; /* current time + 10 seconds */
---  v_rt_date  number;
---  v_rt_time  number;
---begin
---  v_start := localtimestamp;
---  for i in 1 .. v_iterator loop
---    if sysdate <= v_end_date then null; end if;
---  end loop;
---  v_rt_date := console.get_runtime_seconds(v_start);
---  --
---  v_start := localtimestamp;
---  for i in 1 .. v_iterator loop
---    if dbms_utility.get_time <= v_end_time then null; end if;
---  end loop;
---  v_rt_time := console.get_runtime_seconds(v_start);
---  --
---  console.print( '- date        : ' || trim(to_char(v_rt_date, '0.000000')) || ' seconds' );
---  console.print( '- get_time    : ' || trim(to_char(v_rt_time, '0.000000')) || ' seconds' );
---  console.print( '- factor      : ' || trim(to_char(v_rt_date/v_rt_time, '90.0')));
---end;
---/
---
---prompt
---prompt BOOLEAN > INTEGER - 100.000 CALLS
---declare
---  v_iterator pls_integer := 100000;
---  v_start    timestamp;
---  v_boolean  boolean     := true;
---  v_integer  pls_integer := 1;
---  v_rt_bool  number;
---  v_rt_int   number;
---begin
---  v_start := localtimestamp;
---  for i in 1 .. v_iterator loop
---    if v_boolean then null; end if;
---  end loop;
---  v_rt_bool := console.get_runtime_seconds(v_start);
---  --
---  v_start := localtimestamp;
---  for i in 1 .. v_iterator loop
---    if v_integer = 1 then null; end if;
---  end loop;
---  v_rt_int := console.get_runtime_seconds(v_start);
---  --
---  console.print( '- boolean     : ' || trim(to_char(v_rt_bool, '0.000000')) || ' seconds' );
---  console.print( '- integer     : ' || trim(to_char(v_rt_int, '0.000000')) || ' seconds' );
---  console.print( '- factor      : ' || trim(to_char(v_rt_bool/v_rt_int, '90.0')));
---end;
---/
---
---prompt
---prompt RUNTIME: REGEX > EXTRACT > SUBSTR - 100.000 CALLS
---declare
---  v_iterator   pls_integer := 100000;
---  v_start      timestamp;
---  v_temp       varchar2(20);
---  v_rt_extract number;
---  v_rt_regex   number;
---  v_rt_substr  number;
---  --
---  function util_runtime_regex (p_start timestamp) return varchar2 is
---  begin
---    return regexp_substr(to_char(localtimestamp - p_start), '\d{2}:\d{2}:\d{2}\.\d{6}');
---  end util_runtime_regex;
---  --
---  function util_runtime_extract (p_start timestamp) return varchar2 is
---    v_runtime interval day to second (6);
---  begin
---    v_runtime := localtimestamp - p_start;
---    return
---      trim(to_char(extract(hour   from v_runtime), '00'       )) || ':' ||
---      trim(to_char(extract(minute from v_runtime), '00'       )) || ':' ||
---      trim(to_char(extract(second from v_runtime), '00D000000')) ;
---  end util_runtime_extract;
---  --
---  function util_runtime_substr (p_start timestamp) return varchar2 is
---    v_runtime varchar2(32);
---  begin
---    v_runtime := to_char(localtimestamp - p_start);
---    return substr(v_runtime, instr(v_runtime,':')-2, 15);
---  end util_runtime_substr;
---  --
---begin
---  v_start := localtimestamp;
---  for i in 1 .. v_iterator loop
---    v_temp := util_runtime_regex(v_start);
---  end loop;
---  v_rt_regex := console.get_runtime_seconds(v_start);
---  --
---  v_start := localtimestamp;
---  for i in 1 .. v_iterator loop
---    v_temp := util_runtime_extract(v_start);
---  end loop;
---  v_rt_extract := console.get_runtime_seconds(v_start);
---  --
---  v_start := localtimestamp;
---  for i in 1 .. v_iterator loop
---    v_temp := util_runtime_substr(v_start);
---  end loop;
---  v_rt_substr := console.get_runtime_seconds(v_start);
---  --
---  console.print( '- regex       : ' || trim(to_char(v_rt_regex,   '0.000000')) || ' seconds' );
---  console.print( '- exract      : ' || trim(to_char(v_rt_extract, '0.000000')) || ' seconds' );
---  console.print( '- substr      : ' || trim(to_char(v_rt_substr,  '0.000000')) || ' seconds' );
---  console.print( '- factor r/s  : ' || trim(to_char(v_rt_regex/v_rt_substr, '90.0')));
---  console.print( '- factor e/s  : ' || trim(to_char(v_rt_extract/v_rt_substr, '90.0')));
---end;
---/
+prompt
+prompt TIMESTAMP > DATE - 100.000 CALLS
+declare
+  v_iterator pls_integer := 100000;
+  v_start    timestamp;
+  v_end_time timestamp   := systimestamp + 1/24/60*10;
+  v_end_date date        := sysdate      + 1/24/60*10;
+  v_rt_time  number;
+  v_rt_date  number;
+begin
+  v_start := localtimestamp;
+  for i in 1 .. v_iterator loop
+    if systimestamp <= v_end_time then null; end if;
+  end loop;
+  v_rt_time := console.get_runtime_seconds(v_start);
+  --
+  v_start := localtimestamp;
+  for i in 1 .. v_iterator loop
+    if sysdate <= v_end_date then null; end if;
+  end loop;
+  v_rt_date := console.get_runtime_seconds(v_start);
+  --
+  console.print( '- timestamp      : ' || trim(to_char(v_rt_time, '0.000000')) || ' seconds' );
+  console.print( '- date           : ' || trim(to_char(v_rt_date, '0.000000')) || ' seconds' );
+  console.print( '- factor         : ' || trim(to_char(v_rt_time/v_rt_date, '90.0')));
+end;
+/
+
+prompt
+prompt DATE > GET_TIME - 100.000 CALLS
+declare
+  v_iterator pls_integer := 100000;
+  v_start    timestamp;
+  v_end_date date        := sysdate               + 1/24/60*10;
+  v_end_time pls_integer := dbms_utility.get_time + 100 * 10; --current time + 10 seconds
+  v_rt_date  number;
+  v_rt_time  number;
+begin
+  v_start := localtimestamp;
+  for i in 1 .. v_iterator loop
+    if sysdate <= v_end_date then null; end if;
+  end loop;
+  v_rt_date := console.get_runtime_seconds(v_start);
+  --
+  v_start := localtimestamp;
+  for i in 1 .. v_iterator loop
+    if dbms_utility.get_time <= v_end_time then null; end if;
+  end loop;
+  v_rt_time := console.get_runtime_seconds(v_start);
+  --
+  console.print( '- date           : ' || trim(to_char(v_rt_date, '0.000000')) || ' seconds' );
+  console.print( '- get_time       : ' || trim(to_char(v_rt_time, '0.000000')) || ' seconds' );
+  console.print( '- factor         : ' || trim(to_char(v_rt_date/v_rt_time, '90.0')));
+end;
+/
+
+prompt
+prompt BOOLEAN > INTEGER - 100.000 CALLS
+declare
+  v_iterator pls_integer := 100000;
+  v_start    timestamp;
+  v_boolean  boolean     := true;
+  v_integer  pls_integer := 1;
+  v_rt_bool  number;
+  v_rt_int   number;
+begin
+  v_start := localtimestamp;
+  for i in 1 .. v_iterator loop
+    if v_boolean then null; end if;
+  end loop;
+  v_rt_bool := console.get_runtime_seconds(v_start);
+  --
+  v_start := localtimestamp;
+  for i in 1 .. v_iterator loop
+    if v_integer = 1 then null; end if;
+  end loop;
+  v_rt_int := console.get_runtime_seconds(v_start);
+  --
+  console.print( '- boolean        : ' || trim(to_char(v_rt_bool, '0.000000')) || ' seconds' );
+  console.print( '- integer        : ' || trim(to_char(v_rt_int, '0.000000')) || ' seconds' );
+  console.print( '- factor         : ' || trim(to_char(v_rt_bool/v_rt_int, '90.0')));
+end;
+/
+
+prompt
+prompt RUNTIME: REGEX > EXTRACT > SUBSTR - 100.000 CALLS
+declare
+  v_iterator   pls_integer := 100000;
+  v_start      timestamp;
+  v_temp       varchar2(20);
+  v_rt_extract number;
+  v_rt_regex   number;
+  v_rt_substr  number;
+  --
+  function util_runtime_regex (p_start timestamp) return varchar2 is
+  begin
+    return regexp_substr(to_char(localtimestamp - p_start), '\d{2}:\d{2}:\d{2}\.\d{6}');
+  end util_runtime_regex;
+  --
+  function util_runtime_extract (p_start timestamp) return varchar2 is
+    v_runtime interval day to second (6);
+  begin
+    v_runtime := localtimestamp - p_start;
+    return
+      trim(to_char(extract(hour   from v_runtime), '00'       )) || ':' ||
+      trim(to_char(extract(minute from v_runtime), '00'       )) || ':' ||
+      trim(to_char(extract(second from v_runtime), '00D000000')) ;
+  end util_runtime_extract;
+  --
+  function util_runtime_substr (p_start timestamp) return varchar2 is
+    v_runtime varchar2(32);
+  begin
+    v_runtime := to_char(localtimestamp - p_start);
+    return substr(v_runtime, instr(v_runtime,':')-2, 15);
+  end util_runtime_substr;
+  --
+begin
+  v_start := localtimestamp;
+  for i in 1 .. v_iterator loop
+    v_temp := util_runtime_regex(v_start);
+  end loop;
+  v_rt_regex := console.get_runtime_seconds(v_start);
+  --
+  v_start := localtimestamp;
+  for i in 1 .. v_iterator loop
+    v_temp := util_runtime_extract(v_start);
+  end loop;
+  v_rt_extract := console.get_runtime_seconds(v_start);
+  --
+  v_start := localtimestamp;
+  for i in 1 .. v_iterator loop
+    v_temp := util_runtime_substr(v_start);
+  end loop;
+  v_rt_substr := console.get_runtime_seconds(v_start);
+  --
+  console.print( '- regex          : ' || trim(to_char(v_rt_regex,   '0.000000')) || ' seconds' );
+  console.print( '- exract         : ' || trim(to_char(v_rt_extract, '0.000000')) || ' seconds' );
+  console.print( '- substr         : ' || trim(to_char(v_rt_substr,  '0.000000')) || ' seconds' );
+  console.print( '- factor r/s     : ' || trim(to_char(v_rt_regex/v_rt_substr, '90.0')));
+  console.print( '- factor e/s     : ' || trim(to_char(v_rt_extract/v_rt_substr, '90.0')));
+end;
+/
+*/
