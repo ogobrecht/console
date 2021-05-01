bc738a9003cbb3186b4f34a90774d95f88735c57
Ottmar Gobrecht <ogobrecht@users.noreply.github.com>
2021-05-01 17:20:38 +0200

    remove one compiler flag

--- tests/ccflags.sql
+++ tests/ccflags.sql
@@ -9,64 +9,32 @@ alter session set plsql_warnings = 'enable:all,disable:5004,disable:6005,disable
 alter session set plscope_settings = 'identifiers:all';
 alter session set plsql_optimize_level = 3;
 
-prompt - SET COMPILER FLAGS TO APEX_INSTALLED:TRUE, APEX_FUN:TRUE, UTILS_PUBLIC:TRUE
-alter session set plsql_ccflags = 'apex_installed:true, apex_fun:true, utils_public:true';
+prompt - SET COMPILER FLAGS TO APEX_INSTALLED:TRUE, UTILS_PUBLIC:TRUE
+alter session set plsql_ccflags = 'apex_installed:true, utils_public:true';
 prompt - Compile package console (spec)
 @sources/CONSOLE.pks
 prompt - Compile package console (body)
 @sources/CONSOLE.pkb
 @tests/ccflags_check_for_error
 
-prompt - SET COMPILER FLAGS TO APEX_INSTALLED:TRUE, APEX_FUN:TRUE, UTILS_PUBLIC:FALSE
-alter session set plsql_ccflags = 'apex_installed:true, apex_fun:true, utils_public:false';
+prompt - SET COMPILER FLAGS TO APEX_INSTALLED:TRUE, UTILS_PUBLIC:FALSE
+alter session set plsql_ccflags = 'apex_installed:true, utils_public:false';
 prompt - Compile package console (spec)
 @sources/CONSOLE.pks
 prompt - Compile package console (body)
 @sources/CONSOLE.pkb
 @tests/ccflags_check_for_error
 
-prompt - SET COMPILER FLAGS TO APEX_INSTALLED:TRUE, APEX_FUN:FALSE, UTILS_PUBLIC:FALSE
-alter session set plsql_ccflags = 'apex_installed:true, apex_fun:false, utils_public:false';
+prompt - SET COMPILER FLAGS TO APEX_INSTALLED:FALSE, UTILS_PUBLIC:TRUE
+alter session set plsql_ccflags = 'apex_installed:false, utils_public:true';
 prompt - Compile package console (spec)
 @sources/CONSOLE.pks
 prompt - Compile package console (body)
 @sources/CONSOLE.pkb
 @tests/ccflags_check_for_error
 
-prompt - SET COMPILER FLAGS TO APEX_INSTALLED:TRUE, APEX_FUN:FALSE, UTILS_PUBLIC:TRUE
-alter session set plsql_ccflags = 'apex_installed:true, apex_fun:false, utils_public:true';
-prompt - Compile package console (spec)
-@sources/CONSOLE.pks
-prompt - Compile package console (body)
-@sources/CONSOLE.pkb
-@tests/ccflags_check_for_error
-
-prompt - SET COMPILER FLAGS TO APEX_INSTALLED:FALSE, APEX_FUN:FALSE, UTILS_PUBLIC:FALSE
-alter session set plsql_ccflags = 'apex_installed:false, apex_fun:false, utils_public:false';
-prompt - Compile package console (spec)
-@sources/CONSOLE.pks
-prompt - Compile package console (body)
-@sources/CONSOLE.pkb
-@tests/ccflags_check_for_error
-
-prompt - SET COMPILER FLAGS TO APEX_INSTALLED:FALSE, APEX_FUN:FALSE, UTILS_PUBLIC:TRUE
-alter session set plsql_ccflags = 'apex_installed:false, apex_fun:false, utils_public:true';
-prompt - Compile package console (spec)
-@sources/CONSOLE.pks
-prompt - Compile package console (body)
-@sources/CONSOLE.pkb
-@tests/ccflags_check_for_error
-
-prompt - SET COMPILER FLAGS TO APEX_INSTALLED:FALSE, APEX_FUN:TRUE, UTILS_PUBLIC:TRUE
-alter session set plsql_ccflags = 'apex_installed:false, apex_fun:true, utils_public:true';
-prompt - Compile package console (spec)
-@sources/CONSOLE.pks
-prompt - Compile package console (body)
-@sources/CONSOLE.pkb
-@tests/ccflags_check_for_error
-
-prompt - SET COMPILER FLAGS TO APEX_INSTALLED:FALSE, APEX_FUN:TRUE, UTILS_PUBLIC:FALSE
-alter session set plsql_ccflags = 'apex_installed:false, apex_fun:true, utils_public:false';
+prompt - SET COMPILER FLAGS TO APEX_INSTALLED:FALSE, UTILS_PUBLIC:FALSE
+alter session set plsql_ccflags = 'apex_installed:false, utils_public:false';
 prompt - Compile package console (spec)
 @sources/CONSOLE.pks
 prompt - Compile package console (body)
