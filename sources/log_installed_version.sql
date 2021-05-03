345fe67699f4949b46d02a2a95a42a91f1514f70
Ottmar Gobrecht <ogobrecht@users.noreply.github.com>
2021-05-03 13:38:00 +0200

    use direct insert to be independent of current log level

--- sources/log_installed_version.sql
+++ sources/log_installed_version.sql
@@ -10,7 +10,20 @@ begin
   if v_count = 0 then
     -- without execute immediate this script will raise an error when the package console is not valid
     execute immediate 'select console.version from dual' into v_console_version;
-    execute immediate q'[begin console.info('{o,o} CONSOLE v]' || v_console_version || q'[ installed'); end;]';
+    execute immediate replace( q'[
+      -- We are doing a direct insert to create a log entry independent of the current log level.
+      declare
+        v_row console_logs%rowtype;
+      begin
+        v_row.log_systime := systimestamp;
+        v_row.level_id    := 3;
+        v_row.level_name  := console.get_level_name(3);
+        v_row.permanent   := 'Y';
+        v_row.scope       := 'Library Installation';
+        v_row.message     := '{o,o} CONSOLE v#CONSOLE_VERSION# installed';
+        insert into console_logs values v_row;
+        commit;
+      end;]', '#CONSOLE_VERSION#', v_console_version);
     dbms_output.put_line('>           ');
     dbms_output.put_line('>   .___.   ');
     dbms_output.put_line('>   {o,o}   ');
