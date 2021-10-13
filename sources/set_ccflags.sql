e75ca815d27d37a607e12adb1297f0bd5e637742
Ottmar Gobrecht <ogobrecht@users.noreply.github.com>
2021-10-13 11:06:07 +0200

    add compiler flag for native compilation

--- sources/set_ccflags.sql
+++ sources/set_ccflags.sql
@@ -2,15 +2,19 @@
 
 prompt - Set compiler flags
 declare
-  v_apex_installed varchar2(5) := 'FALSE'; -- Do not change (is set dynamically).
-  v_utils_public   varchar2(5) := 'FALSE'; -- Make utilities public available (for testing or other usages).
+  v_apex_installed     varchar2(5) := 'FALSE'; -- Do not change (is set dynamically).
+  v_utils_public       varchar2(5) := 'FALSE'; -- Make utilities public available (for testing or other usages).
+  v_native_compilation boolean     := false;   -- Set this to true on your own risk (in the Oracle cloud you will get likely an "insufficient privileges" error)
   v_count pls_integer;
 begin
 
-  --Basic settings
   execute immediate 'alter session set plsql_warnings = ''enable:all,disable:5004,disable:6005,disable:6006,disable:6009,disable:6010,disable:6027''';
   execute immediate 'alter session set plscope_settings = ''identifiers:all''';
   execute immediate 'alter session set plsql_optimize_level = 3';
+
+  if v_native_compilation then
+    execute immediate 'alter session set plsql_code_type=''native''';
+  end if;
 
   select count(*) into v_count from all_objects where object_type = 'SYNONYM' and object_name = 'APEX_EXPORT';
   v_apex_installed := case when v_count = 0 then 'FALSE' else 'TRUE' end;
