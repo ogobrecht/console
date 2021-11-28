3e0c3d455e237a5cd956f835cd9d6c733ed0bde9
Ottmar Gobrecht <ogobrecht@users.noreply.github.com>
2021-11-28 15:08:56 +0100

    fix ccflags for tests

--- tests/ccflags_check_for_error.sql
+++ tests/ccflags_check_for_error.sql
@@ -21,3 +21,16 @@ begin
   end if;
 end;
 /
+
+column "Name"      format a15
+column "Line,Col"  format a10
+column "Type"      format a10
+column "Message"   format a80
+
+select name || case when type like '%BODY' then ' body' end as "Name",
+       line || ',' || position as "Line,Col",
+       attribute               as "Type",
+       text                    as "Message"
+  from user_errors
+ where name = 'CONSOLE'
+ order by name, line, position;
